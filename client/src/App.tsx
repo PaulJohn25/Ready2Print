34dfd0ffe50c86d82bcb67c717371fe5bb472c73
John Paul Imperial <121214456+PaulJohn25@users.noreply.github.com>
2025-01-03 11:19:00 +0800

    Remove updates on App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,27 +1,6 @@
 import Home from "./pages/Home/Home";
-import { useEffect } from "react";
 
 function App() {
-  const isMessenger = () => {
-    const userAgent = navigator.userAgent;
-    return /FBAN|FBAV|Instagram/.test(userAgent);
-  };
-
-  useEffect(() => {
-    // If the user is in Messenger, redirect to Google search with your website URL
-    if (isMessenger()) {
-      const websiteUrl = "https://ready2-print.vercel.app/"; // Your website URL
-      const googleSearchUrl = `https://www.google.com/search?q=${encodeURIComponent(
-        websiteUrl
-      )}`;
-      window.location.href = googleSearchUrl; // Redirect to Google search with the website URL as a query
-    }
-  }, []); // Empty dependency array ensures this runs once when the component mounts
-
-  useEffect(() => {
-    document.title = "PDF Editor";
-  }, []);
-
   return (
     <>
       <Home />
