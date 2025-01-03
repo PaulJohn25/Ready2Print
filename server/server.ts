70eb5a961a309ffc52344e3a78cce77c8fc6acf1
John Paul Imperial <121214456+PaulJohn25@users.noreply.github.com>
2025-01-03 11:50:54 +0800

    Add .env file on server
    Create test email sender

--- server/server.ts
+++ server/server.ts
@@ -1,6 +1,43 @@
 import express, { Request, Response } from "express";
+import multer from "multer";
+import nodemailer from "nodemailer";
+import bodyParser from "body-parser";
+// import path from "path";
+// import fs from "fs";
+
+require("dotenv").config();
 
 const app = express();
+const upload = multer({ dest: "uploads/" });
+
+// Middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// Nodemailer setup
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASSWORD,
+  },
+});
+
+transporter.sendMail(
+  {
+    from: process.env.EMAIL_USER,
+    to: "johnpaulnidua@gmail.com",
+    subject: "Test Email",
+    text: "This is a test email",
+  },
+  (error, info) => {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log("Email sent: " + info.response);
+    }
+  }
+);
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Server is running...");
