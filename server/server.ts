c2c994d660c0b7dd4ce20aa248d44ec14b168f28
John Paul Imperial <121214456+PaulJohn25@users.noreply.github.com>
2025-01-03 10:34:46 +0800

    Update server vercel.json

--- server/server.ts
+++ server/server.ts
@@ -1,11 +1,13 @@
-import express from "express";
+import express, { Request, Response } from "express";
 
 const app = express();
 
-app.get("/api", (req, res) => {
+app.get("/api", (req: Request, res: Response) => {
   res.json({ users: ["userOne", "userTwo", "userThree"] });
 });
 
 app.listen(5000, () => {
   console.log("Server is running on port 5000");
 });
+
+export default app;
