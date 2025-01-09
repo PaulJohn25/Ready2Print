a817a4df0a18e1078ecc6409d0247e8c3be4d49c
John Paul Imperial <121214456+PaulJohn25@users.noreply.github.com>
2025-01-09 09:56:14 +0800

    Add get method

--- server/server.ts
+++ server/server.ts
@@ -145,6 +145,11 @@ app.post(
   }
 );
 
+app.get("/", (req: Request, res: Response) => {
+  console.log("Server is running....");
+  res.send("Server is running...");
+});
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
